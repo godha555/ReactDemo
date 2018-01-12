65fa6231a62d5a06d6457f6d7332b8b7ffd4c766
godha555 <s528153@mail.nwmissouri.edu>
2018-01-12 03:04:00 -0500

    random

--- my-app/src/DemoComponent.js
+++ my-app/src/DemoComponent.js
@@ -9,10 +9,10 @@ class DemoComponent extends Component {
            <div class="panel panel-default">
              <div class="panel-heading">
                 <h4 class="panel-title">
-                  <a data-toggle="collapse" data-parent="#accordion" href={this.props.value1}> {this.props.value}</a>
+                  <a data-toggle="collapse" data-parent="#accordion" href="#collapse1"> {this.props.value}</a>
                 </h4>
               </div>
-              <div id={this.props.value1} class="panel-collapse collapse in">
+              <div id="collapse1" class="panel-collapse collapse in">
                 <div class="panel-body"> {this.props.phone}</div>
                 </div>
               </div>
